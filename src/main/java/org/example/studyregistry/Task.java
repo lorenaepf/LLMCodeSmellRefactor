cdc7ddc3f375299e5bc4a6a2a009ed820e4aadd7
Robsu66 <robsondiaswp@gmail.com>
2025-01-12 21:56:49 -0300

    fix: data refactor code smell Data Class using the Move Method refactoring technique

--- src/main/java/org/example/studyregistry/Task.java
+++ src/main/java/org/example/studyregistry/Task.java
@@ -2,7 +2,7 @@ package org.example.studyregistry;
 
 import java.time.LocalDateTime;
 
-public class Task extends Registry{
+public class Task extends Registry {
     private String title;
     private String description;
     private String author;
@@ -10,33 +10,69 @@ public class Task extends Registry{
 
     public Task(String title, String description, String author, LocalDateTime date) {
         this.title = title;
-        this.name = title;
+        this.name = title; // Compatibilidade com a superclasse (Registry)
         this.description = description;
         this.author = author;
         this.date = date;
     }
+
+    // Métodos de acesso para compatibilidade com testes
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getAuthor() {
         return author;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
+
     public LocalDateTime getDate() {
         return date;
     }
+
     public void setDate(LocalDateTime date) {
         this.date = date;
+    }
+
+    // Adicionando lógica relevante para encapsular comportamentos
+    public boolean isOverdue() {
+        return date.isBefore(LocalDateTime.now());
+    }
+
+    public void postponeTask(int days) {
+        this.date = this.date.plusDays(days);
+    }
+
+    public String getSummary() {
+        return String.format(
+                "Task '%s' by %s. Due on %s. Description: %s",
+                title,
+                author,
+                date.toString(),
+                description
+        );
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Task{title='%s', description='%s', author='%s', date=%s}",
+                title, description, author, date
+        );
     }
 }
